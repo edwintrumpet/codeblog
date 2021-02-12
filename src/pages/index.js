5a042536d98a264e851331e557cb66f2d4c629b0
edwin <47165886+edwintrumpet@users.noreply.github.com>
2021-02-12 00:17:52 -0500

    [feature] Add not found page and under construction message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,13 @@
-import { Button } from 'antd';
+import { ToolOutlined } from '@ant-design/icons';
+import { Result } from 'antd';
 import React from 'react';
 
 export default function index() {
-  return <Button type="primary">Primary</Button>;
+  return (
+    <Result
+      icon={<ToolOutlined />}
+      status="warning"
+      title="Sitio en construcción"
+    />
+  );
 }
